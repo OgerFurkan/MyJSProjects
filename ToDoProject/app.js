d55de4bc6711d92239c510963be62240a96019ee
Furkan Öger <2021123051@cumhuriyet.edu.tr>
2025-02-08 15:48:52 +0300

    I am making a todo website - 2
    Today, Ive added deleting a todo or all todos from both the UI and localStorage, and searching for a ToDo.

--- ToDoProject/app.js
+++ ToDoProject/app.js
@@ -1,6 +1,9 @@
 const todoList = document.querySelectorAll('.card-body')[1].children[2];
 const firstCardBody = document.querySelectorAll('.card-body')[0];
+const secondCardBody = document.querySelectorAll('.card-body')[1];
 const addButton = document.querySelector('#todoAddButton');
+const deleteButton = document.querySelector('#todoClearButton');
+const searchInput = document.querySelector('#todoSearch');
 const toDoTitle = document.querySelector('#toDoTitle');
 const toDoDesc = document.querySelector('#toDoDesc');
 
@@ -16,7 +19,7 @@ function addButtonAlert(message, type) {
     div.className = `alert alert-${type}`;
     div.role = 'alert';
     div.textContent = message;
-    
+
     firstCardBody.appendChild(div);
     firstCardBody.appendChild(hr);
     let opacity = 1;
@@ -29,14 +32,36 @@ function addButtonAlert(message, type) {
                 opacity -= 0.05;
                 div.style.opacity = opacity;
             }
-        }, 50); 
-    }, 1000); 
+        }, 50);
+    }, 1000);
 }
+function deleteButtonAlert(message, type) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${type}`;
+    div.role = 'alert';
+    div.textContent = message;
 
+    secondCardBody.children[1].after(div);
+    let opacity = 1;
+    setTimeout(() => {
+        let fadeEffect = setInterval(() => {
+            if (opacity <= 0) {
+                clearInterval(fadeEffect);
+                div.style.display = "none";
+            } else {
+                opacity -= 0.05;
+                div.style.opacity = opacity;
+            }
+        }, 50);
+    }, 1000);
+}
 
 function run() {
     addButton.addEventListener('click', addTodo);
     document.addEventListener('DOMContentLoaded', pageLoad);
+    secondCardBody.addEventListener('click', deleteToDoUI);
+    deleteButton.addEventListener('click', deleteAllToDo);
+    searchInput.addEventListener('keyup', searchToDo);
 }
 
 function addTodo(e) {
@@ -53,9 +78,15 @@ function addTodo(e) {
     let date = new Date();
     addNewToDoUI(title, desc, date.toLocaleDateString());
     addButtonAlert('Başarıyla eklendi!', 'success');
+
+    setTimeout(() => {
+        location.reload();
+    }, 1000);
+
     addNewToDoStorage(title, desc, date.toLocaleDateString());
 
-    e.preventDefault(); // Formun sayfayı yenilemesini engeller.
+
+    e.preventDefault();
 }
 
 function addNewToDoUI(title, desc, day) {
@@ -67,7 +98,7 @@ function addNewToDoUI(title, desc, day) {
 
     const divHeader = document.createElement('div');
     divHeader.className = 'card-header';
-    divHeader.textContent = title.toUpperCase();
+    divHeader.textContent = title.toUpperCase('tr');
 
     const a = document.createElement('a');
     a.className = 'delete-item';
@@ -127,7 +158,60 @@ function setLocalStorage() {
 function pageLoad() {
     setLocalStorage();
     toDoHeader.forEach((header, index) => {
-        addNewToDoUI(index+1+" - "+header, toDoBody[index], toDoFooter[index]);
+        addNewToDoUI(index + 1 + " - " + header, toDoBody[index], toDoFooter[index]);
     });
 }
 
+function deleteToDoUI(e) {
+    if (e.target.className === 'fa fa-remove') {
+        const deletedToDo = e.target.parentElement.parentElement.parentElement.parentElement
+        deletedToDo.remove();
+        deleteButtonAlert('Başarıyla silindi!', 'success');
+        deleteToDoStorage(deletedToDo);
+    }
+}
+
+function deleteToDoStorage(removeToDo) {
+    setLocalStorage();
+    toDoHeader.forEach((header, index) => {
+        if (removeToDo.children[0].children[0].textContent.trim().charAt(0) == index + 1) {
+            toDoHeader.splice(index, 1);
+            toDoBody.splice(index, 1);
+            toDoFooter.splice(index, 1);
+        }
+    });
+    localStorage.setItem("toDoHeader", JSON.stringify(toDoHeader));
+    localStorage.setItem("toDoBody", JSON.stringify(toDoBody));
+    localStorage.setItem("toDoFooter", JSON.stringify(toDoFooter));
+}
+
+function deleteAllToDo() {
+    if (todoList.children.length > 0) {
+        if (confirm('Tümünü silmek istediğinize emin misiniz?')) {
+            while (todoList.firstChild != null) {
+                todoList.removeChild(todoList.firstChild);
+            }
+            deleteButtonAlert('Başarıyla silindi!', 'success');
+            localStorage.clear();
+            setLocalStorage();
+        }
+    }
+    else {
+        deleteButtonAlert('Silme işlemi için en az 1 yapılacak olması lazım!', 'warning');
+    }
+}
+
+function searchToDo(e) {
+    const listItems = document.querySelectorAll('.list-group-item');
+    const searchValue = e.target.value.toLowerCase('tr').trim();
+    listItems.forEach((listItem) => {
+        const tempHeader = listItem.children[0].children[0].textContent.toLowerCase('tr');
+        const tempBody = listItem.children[0].children[1].children[0].textContent.toLowerCase('tr');
+        if (tempHeader.includes(searchValue) || tempBody.includes(searchValue)) {
+            listItem.setAttribute('style', 'display: flex !important');
+        }
+        else {
+            listItem.setAttribute('style', 'display: none !important');
+        }
+    });
+}
